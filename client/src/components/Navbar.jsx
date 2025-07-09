fb69d3c9a0467ce71e31ffee9bb5424e8354bdfb
manish <singhmanish9720@gmail.com>
2025-07-09 09:26:54 +0530

    form on nav

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -35,8 +35,18 @@ const Navbar = (setshowlogin) => {
      <button className='sm:hidden cursor-pointer' aria-label="menu" onClick={()=>setOpen(!open)}>
       <img src={open ? assets.close_icon: assets.menu_icon} alt="" />
      </button>
+  
+  {/* not yet */}
+    <div>
+    <form action="">
+
+    </form>
+    </div>
+{/* not yet */}
 
     </div>
+
+    
   )
 }
 
